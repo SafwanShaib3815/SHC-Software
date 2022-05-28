9ecf7118aaf2d180828210fdaaf2f887e79655dc
TanushreeRay0428 <tanushree.ray1989@gmail.com>
2022-05-28 00:48:15 -0400

    Implemented bottom Navigation for the Smart Home Controller Project

--- SmartHomeControllerT3/src/main/java/ca/T3/fab4/it/smart/home/controller/T3MainActivity.java
+++ SmartHomeControllerT3/src/main/java/ca/T3/fab4/it/smart/home/controller/T3MainActivity.java
@@ -4,11 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class T3MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private TemperatureFragment temperatureFragment;
+    private MotionFragment motionFragment;
+    private SmokeFragment smokeFragment;
+    private RFIDFragment rfidFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.BTMNAV);
+
+        temperatureFragment = new TemperatureFragment();
+        motionFragment = new MotionFragment();
+        smokeFragment = new SmokeFragment();
+        rfidFragment = new RFIDFragment();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.MENU2:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT, motionFragment).commit();
+                    return true;
+
+                case R.id.MENU3:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT, smokeFragment).commit();
+                    return true;
+
+                case R.id.MENU4:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT, rfidFragment).commit();
+                    return true;
+
+                default:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT,temperatureFragment).commit();
+                    return true;
+
+            }
+
+        });
+     }
     }
-}
+
