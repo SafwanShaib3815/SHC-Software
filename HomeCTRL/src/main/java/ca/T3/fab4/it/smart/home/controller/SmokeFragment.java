1f60ba5db8c73255abf6decbd1d0f66ad5a17700
NkeiruJohnson1707 <nkeiru.jachilike@gmail.com>
2022-05-29 16:25:14 -0400

    Add all team members name to xml and java files

--- HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/SmokeFragment.java
+++ HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/SmokeFragment.java
@@ -1,8 +1,8 @@
 /*
 Abdulrhman Ragab    n01440938    0NA
 Tanushree Ray    n01440938    0NA
-Safwan Shaib        0NA
-Nkeiru Johnson-Achilike        0NA
+Safwan Shaib    n01343815    0NA
+Nkeiru Johnson-Achilike   n01411707 0NA
 */
 package ca.T3.fab4.it.smart.home.controller;
 
