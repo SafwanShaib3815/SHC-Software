708d491a549327fce1fa79e965d7738f5b1187a5
NkeiruJohnson1707 <nkeiru.jachilike@gmail.com>
2022-05-29 23:36:52 -0400

    Add a new docs directory at project level and pasted pdf deliverable1. Attached pdf copy too

--- HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/RFIDFragment.java
+++ HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/RFIDFragment.java
@@ -14,15 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RFIDFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RFIDFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -34,16 +27,7 @@ public class RFIDFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RFIDFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RFIDFragment newInstance(String param1, String param2) {
+   public static RFIDFragment newInstance(String param1, String param2) {
         RFIDFragment fragment = new RFIDFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
