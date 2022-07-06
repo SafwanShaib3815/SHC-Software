bc34f38657a2fafcff7e289a7b86fe9bcbe0212c
Abdulrhman Ragab <munrvpt@gmail.com>
2022-07-05 22:39:46 -0400

    added snackbar to menu options

--- HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/T3MainActivity.java
+++ HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/T3MainActivity.java
@@ -63,25 +63,25 @@ public class T3MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.MENU2:
                     getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT, motionFragment).commit();
-                    Toast toast = Toast.makeText(getApplicationContext(), "motion screen", Toast.LENGTH_SHORT); // initiate the Toast with context, message and duration for the Toast
+                    Toast toast = Toast.makeText(getApplicationContext(), R.string.motionscreen, Toast.LENGTH_SHORT); // initiate the Toast with context, message and duration for the Toast
                     toast.show();
                     return true;
 
                 case R.id.MENU3:
                     getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT, smokeFragment).commit();
-                    Toast toast1 = Toast.makeText(getApplicationContext(), "smoke screen", Toast.LENGTH_SHORT); // initiate the Toast with context, message and duration for the Toast
+                    Toast toast1 = Toast.makeText(getApplicationContext(), R.string.smokescreen, Toast.LENGTH_SHORT); // initiate the Toast with context, message and duration for the Toast
                     toast1.show();
                     return true;
 
                 case R.id.MENU4:
                     getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT, rfidFragment).commit();
-                    Toast toast2 = Toast.makeText(getApplicationContext(), "rfid screen", Toast.LENGTH_SHORT); // initiate the Toast with context, message and duration for the Toast
+                    Toast toast2 = Toast.makeText(getApplicationContext(), R.string.rfidscreen, Toast.LENGTH_SHORT); // initiate the Toast with context, message and duration for the Toast
                     toast2.show();
                     return true;
 
                 default:
                     getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT, temperatureFragment).commit();
-                    Toast toast3 = Toast.makeText(getApplicationContext(), "temperature screen", Toast.LENGTH_SHORT); // initiate the Toast with context, message and duration for the Toast
+                    Toast toast3 = Toast.makeText(getApplicationContext(), R.string.temperaturescreen, Toast.LENGTH_SHORT); // initiate the Toast with context, message and duration for the Toast
                     toast3.show();
                     return true;
 
@@ -130,14 +130,19 @@ public class T3MainActivity extends AppCompatActivity {
                 makePhoneCall();
                 break;
             case R.id.T3_menu_help:
+                Snackbar.make(findViewById(android.R.id.content), R.string.help, Snackbar.LENGTH_LONG)
+                        .show();
                 break;
-            case R.id.T3_menu_support:
 
+            case R.id.T3_menu_support:
+                Snackbar.make(findViewById(android.R.id.content), R.string.support, Snackbar.LENGTH_LONG)
+                        .show();
                 break;
             case R.id.T3_menu_settings:
                 Intent intent1 = new Intent(T3MainActivity.this,
                         SettingsActivity.class);
                 startActivity(intent1);
+
                 break;
         }
         return super.onOptionsItemSelected(item);
