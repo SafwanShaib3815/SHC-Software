30df4de6b620286a8135a7c3a1368c5cfe00cca7
Nkeiru <nkeiru.jachilike@gmail.com>
2022-07-25 21:40:27 -0400

    implemented the login screen to switch to main screen on successful login

--- HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/LoginActivity.java
+++ HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/LoginActivity.java
@@ -21,5 +21,45 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        b1 = (Button)findViewById(R.id.button1);
+        ed1 = (EditText)findViewById(R.id.login_username);
+        ed2 = findViewById(R.id.login_password);
+
+//        b2 = (Button)findViewById(R.id.button2);
+//        tx1 = (TextView)findViewById(R.id.textView3);
+//        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(LoginActivity.this,T3MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+//        b2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
     }
 }
