5b818f83b748366c16985988f3e4394d924679c8
Nkeiru <nkeiru.jachilike@gmail.com>
2022-07-25 21:29:35 -0400

    Added the login activity to the splash activity

---
+++ HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/LoginActivity.java
@@ -0,0 +1,25 @@
+package ca.T3.fab4.it.smart.home.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button b1,b2;
+    EditText ed1,ed2;
+    TextView tx1;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
