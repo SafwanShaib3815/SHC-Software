cca22ada7dd356b8a4b3a86687ed0fc47bd77961
TanushreeRay0428 <tanushree.ray1989@gmail.com>
2022-07-29 21:32:57 -0400

    Improve the design of the motion screen

--- HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/MotionFragment.java
+++ HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/MotionFragment.java
@@ -7,14 +7,34 @@ Nkeiru Johnson-Achilike   n01411707 0NA
 package ca.T3.fab4.it.smart.home.controller;
 
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MotionFragment extends Fragment {
+
+    String value = "";
+    ImageView imageView;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -48,6 +68,63 @@ public class MotionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_motion, container, false);
+        View view =  inflater.inflate(R.layout.fragment_motion, container, false);
+        Switch readData = view.findViewById(R.id.switch3);
+
+        // Write a message to the database
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference motion = ref.child("Dummy Sensors").child("Motion Sensor").child("Mos");
+        TextView txtMessage = (TextView) view.findViewById(R.id.textView2);
+        imageView = view.findViewById(R.id.imageView);
+        Button onButton = view.findViewById(R.id.button8);
+        Button offButton = view.findViewById(R.id.button9);
+
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.blink);
+                imageView.startAnimation(animation);
+
+            }
+        });
+
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.clearAnimation();
+            }
+        });
+
+        motion.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                value = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        readData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (readData.isChecked()) {
+
+                    txtMessage.setText(value);
+                }else
+                {
+                    txtMessage.setText(" ");
+                }
+
+
+            }
+        });
+        return view;
     }
 }
