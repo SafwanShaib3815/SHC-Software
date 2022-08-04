3283010a2d8e4009a6c65e8776bd785e4b75dd6c
TanushreeRay0428 <tanushree.ray1989@gmail.com>
2022-08-04 03:42:22 -0400

    Fixed the notification screen error

--- HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/NotificationActivity.java
+++ HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/NotificationActivity.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -29,30 +33,52 @@ public class NotificationActivity extends AppCompatActivity {
         notificationLabel.setText("Don't Miss the Notification!!!");
 
         Button btn = findViewById(R.id.button3);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
 
-            NotificationChannel channel = new NotificationChannel("My Channel", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(channel);
-        }
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String CHANNEL_ID = "My Notification";
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(NotificationActivity.this, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_msg)
-                        .setContentTitle("My notification")
-                        .setContentText("Some Unauthorised Activity Encountered")
-                        .setAutoCancel(true);
-
-                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(NotificationActivity.this);
-                managerCompat.notify(1, builder.build());
+                addNotification();
 
             }
         });
 
+    }
+
+    private void addNotification() {
+        NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= 26)
+        {
+            //When sdk version is larger than26
+            String id = "channel_1";
+            String description = "143";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(id, description, importance);
+//                     channel.enableLights(true);
+//                     channel.enableVibration(true);//
+            manager.createNotificationChannel(channel);
+            Notification notification = new Notification.Builder(NotificationActivity.this, id)
+                    .setCategory(Notification.CATEGORY_MESSAGE)
+                    .setSmallIcon(R.drawable.ic_msg)
+                    .setContentTitle("New Alert")
+                    .setContentText("Some unauthorised activity encountered. Action Requited!!!!")
+                    //.setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .build();
+            manager.notify(1, notification);
         }
+        else
+        {
+            //When sdk version is less than26
+            Notification notification = new NotificationCompat.Builder(NotificationActivity.this)
+                    .setContentTitle("New Alert")
+                    .setContentText("Some unauthorised activity encountered. Action Requited!!!!")
+                    //.setContentIntent(pendingIntent)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .build();
+            manager.notify(1,notification);
+        }
+    }
 
     //On action bar back arrow pressed
     @Override
