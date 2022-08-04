77733f3d984f16430910f7d40890d2f472f775fe
sh5safwan <73716969+sh5safwan@users.noreply.github.com>
2022-08-04 01:00:53 -0400

    registration layout and strings modifications plus errors and warnings fixed

--- HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/RegistrationActivity.java
+++ HomeCTRL/src/main/java/ca/T3/fab4/it/smart/home/controller/RegistrationActivity.java
@@ -1,6 +1,5 @@
 package ca.T3.fab4.it.smart.home.controller;
 
-import static android.content.ContentValues.TAG;
 import static java.util.regex.Pattern.compile;
 
 import androidx.annotation.NonNull;
@@ -8,22 +7,13 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.regex.Pattern;
 
@@ -46,79 +36,76 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         mAuth = FirebaseAuth.getInstance();
 
-        fullName = (EditText) findViewById(R.id.name);
-        phone = (EditText) findViewById(R.id.phone);
-        email = (EditText) findViewById(R.id.email);
-        password = (EditText) findViewById(R.id.password);
-        confirmPassword = (EditText) findViewById(R.id.password2);
+        fullName = findViewById(R.id.name);
+        phone = findViewById(R.id.phone);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.password2);
 
         Button register = findViewById(R.id.sign);
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String uName = fullName.getText().toString().trim();
-                String uPhone = phone.getText().toString().trim();
-                String uEmail = email.getText().toString().trim();
-                String uPassword = password.getText().toString().trim();
-                String cPassword = confirmPassword.getText().toString().trim();
+        register.setOnClickListener(v -> {
+            String uName = fullName.getText().toString().trim();
+            String uPhone = phone.getText().toString().trim();
+            String uEmail = email.getText().toString().trim();
+            String uPassword = password.getText().toString().trim();
+            String cPassword = confirmPassword.getText().toString().trim();
 
-                if (uName.isEmpty()) {
-                    fullName.setError("Name Field Can't be Blank!!");
-                    fullName.requestFocus();
-                    return;
-                }
+            if (uName.isEmpty()) {
+                fullName.setError(getString(R.string.reg_name_error));
+                fullName.requestFocus();
+                return;
+            }
 
-                if (uPhone.isEmpty()) {
-                    phone.setError("Phone field can't be empty!!");
-                    phone.requestFocus();
-                    return;
+            if (uPhone.isEmpty()) {
+                phone.setError(getString(R.string.reg_phone_error));
+                phone.requestFocus();
+                return;
 
-
-                }
-                if (uEmail.isEmpty()) {
-                    email.setError("Email field can't be empty!!");
-                    email.requestFocus();
-                    return;
-                }
-                if (!Patterns.EMAIL_ADDRESS.matcher(uEmail).matches()) {
-                    email.setError("Enter a valid email address!!");
-                    email.requestFocus();
-                    return;
-
-                }
-                if (uPassword.isEmpty()) {
-                    password.setError("Phone field can't be empty!!");
-                    password.requestFocus();
-                    return;
-                }
-                else if (!PASSWORD_PATTERN.matcher(uPassword).matches()){
-                    password.setError("Password should have : " + "\n" +
-                            "At least one digit " + "\n" + "At least one upper case letter " + "\n"
-                            + "At least one special charecter ");
-                    password.requestFocus();
-                    return;
-
-                } else {
-                    password.setError(null);
-
-
-                }
-                if (cPassword.isEmpty()) {
-                    confirmPassword.setError("Confirm password can't be empty!!");
-                    confirmPassword.requestFocus();
-                    return;
-                }
-                if (!uPassword.equals(cPassword)) {
-                    confirmPassword.setError("Confirm password should match with the password field!!");
-                    confirmPassword.requestFocus();
-                    return;
-                }
 
             }
+            if (uEmail.isEmpty()) {
+                email.setError(getString(R.string.reg_email_error));
+                email.requestFocus();
+                return;
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(uEmail).matches()) {
+                email.setError(getString(R.string.reg_email_error1));
+                email.requestFocus();
+                return;
+
+            }
+            if (uPassword.isEmpty()) {
+                password.setError(getString(R.string.reg_phone_error1));
+                password.requestFocus();
+                return;
+            }
+            else if (!PASSWORD_PATTERN.matcher(uPassword).matches()){
+                password.setError(getString(R.string.password_should_have) + "\n" +
+                        getString(R.string.at_least_one_digit) + "\n" + getString(R.string.one_upper_case) + "\n"
+                        + getString(R.string.one_special_char));
+                password.requestFocus();
+                return;
+
+            } else {
+                password.setError(null);
+
+
+            }
+            if (cPassword.isEmpty()) {
+                confirmPassword.setError(getString(R.string.reg_conf_pswd_error));
+                confirmPassword.requestFocus();
+                return;
+            }
+            if (!uPassword.equals(cPassword)) {
+                confirmPassword.setError(getString(R.string.reg_conf_pswd_error1));
+                confirmPassword.requestFocus();
+            }
+
         });
 
     }
@@ -126,8 +113,7 @@ public class RegistrationActivity extends AppCompatActivity {
     //On action bar back arrow pressed
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
+        if (item.getItemId() ==  android.R.id.home){
                 this.finish();
                 return true;
         }
